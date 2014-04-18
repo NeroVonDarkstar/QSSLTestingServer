0bf52a5c2401adf7abe02ccc83ff223088584067
NeroVonDarkstar <ScanlonM1@Cardiff.ac.uk>
2014-04-18 22:30:23 +0100

    Amended the code for the server, added cert info

--- main.cpp
+++ main.cpp
@@ -13,31 +13,54 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    QSslSocket serversocket;
-    serversocket.setProtocol(QSsl::SslV3);
-    QByteArray key;
-    QByteArray cert;
-    QFile file_key("www.nerovon.com.key");
-    if(file_key.open(QIODevice::ReadOnly))
-    {
-        key = file_key.readAll();
-        file_key.close();
-    }
-    else
-    {
-        qDebug()<<file_key.errorString();
-    }
 
-    QSslKey ssl_key(key, QSsl::Rsa);
+    //After this is the new code!
+        QSslSocket Socket;
+        Socket.setProtocol(QSsl::SslV3);
 
-    QSslCertificate ssl_cert(cert);
+       QByteArray  key;
+       QByteArray  cert;
 
-    serversocket.setPrivateKey(ssl_key);
-    serversocket.setLocalCertificate(ssl_cert);
-    cout << "Starting the server encryption" << endl;
-    serversocket.startServerEncryption();
-    cout << "Encrypted server successful" << endl;
+       QFile  fileKey("C:\Qt\Tools\QtCreator\bin\QTTestingSSLServer\www.nerovon.com.key");
+       if(fileKey.open(QIODevice ::ReadOnly))
+       {
+           key = fileKey.readAll();
+           fileKey.close();
+       }
+       else
+       {
+           qDebug() << fileKey.errorString();
+       }
+
+       QFile  fileCert("C:\Qt\Tools\QtCreator\bin\QTTestingSSLServer\www.nerovon.com.csr");
+       if(fileCert.open(QIODevice ::ReadOnly))
+       {
+           cert = fileCert.readAll();
+           fileCert.close();
+       }
+       else
+       {
+           qDebug() << fileCert.errorString();
+       }
+
+       qDebug() << key + "\n" + cert;
+
+       QSslKey sslKey(key, QSsl::Rsa);
+       QSslCertificate sslCert(cert);
+
+       Socket.setPrivateKey(sslKey);
+       Socket.setLocalCertificate(sslCert);
+
+       if (!Socket.setSocketDescriptor(SocketDescriptor)) {
+          return 0;
+       }
+
+       Socket.startServerEncryption();
+       cout << "Server successfully created" << endl;
+
+       if(!Socket.waitForEncrypted()) {
+          return 0;
+       }
 
     return a.exec();
 }
